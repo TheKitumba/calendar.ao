21a8828a1b06345fad104017c78164ae1aef8f15
Alberto Kitumba <alberkitumba@gmail.com>
2025-06-19 21:37:32 +0100

    chore: remove unused vars

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,7 +1,7 @@
 export function Calendar() {
   return (
     <div className="grid grid-cols-7  gap-2 px-3 md:px-10 pb-10">
-      {Array.from({ length: 42 }).map((item, index) => (
+      {Array.from({ length: 42 }).map((_, index) => (
         <button className="aspect-square bg-white border flex items-start p-2 md:p-5 text-base md:text-4xl font-black">
           {index}
         </button>
