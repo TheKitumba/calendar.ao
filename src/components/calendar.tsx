1510d0e6f80aa169be95297b6154b3deb296c377
Alberto Kitumba <alberkitumba@gmail.com>
2025-06-20 20:42:15 +0100

    chore: reduce the number of days showned

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,9 +1,9 @@
 export function Calendar() {
   return (
     <div className="grid grid-cols-7  gap-2 px-3 md:px-10 pb-10">
-      {Array.from({ length: 42 }).map((_, index) => (
+      {Array.from({ length: 30 }).map((_, index) => (
         <button className="aspect-square bg-white border flex items-start p-2 md:p-5 text-base md:text-4xl font-black">
-          {index}
+          {index + 1}
         </button>
       ))}
     </div>
