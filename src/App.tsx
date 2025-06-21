e52ab5cbeaf53b28a36b6538cf1ecb3184490cb1
Alberto Kitumba <alberkitumba@gmail.com>
2025-06-21 23:55:05 +0100

    feat: add days of the week navigate to prev and next month

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
 import { Calendar } from "./components/calendar";
 
 export default function App() {
-  return (
-    <div className="h-full flex flex-col gap-8">
-      <div className="space-y-5">
-        <h1 className="text-4xl md:text-7xl font-black text-center mt-20">
-          Caledário.ao
-        </h1>
-        <hr />
-      </div>
-      <Calendar />
-    </div>
-  );
+  return <Calendar />;
 }
--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,11 +1,92 @@
+import {
+  addMonths,
+  eachDayOfInterval,
+  endOfWeek,
+  format,
+  getDaysInMonth,
+  startOfWeek,
+} from "date-fns";
+import { pt, ptBR } from "date-fns/locale";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { useState } from "react";
+
+function getDaysOfTheWeekPT(): string[] {
+  const hoje = new Date();
+  const startWeek = startOfWeek(hoje, { locale: pt });
+  const weekend = endOfWeek(hoje, { locale: pt });
+
+  const days = eachDayOfInterval({
+    start: startWeek,
+    end: weekend,
+  });
+
+  return days.map((dia) => format(dia, "EEEE", { locale: ptBR }));
+}
+
 export function Calendar() {
+  const [date, setDate] = useState(new Date());
+  const daysInMonth = getDaysInMonth(date);
+  const daysOfTheWeek = getDaysOfTheWeekPT();
+
+  const handlePrevMonth = () => {
+    setDate((prev) => {
+      return addMonths(prev, -1);
+    });
+  };
+
+  const handleNextMonth = () => {
+    setDate((prev) => {
+      return addMonths(prev, 1);
+    });
+  };
+
   return (
-    <div className="grid grid-cols-7  gap-2 px-3 md:px-10 pb-10">
-      {Array.from({ length: 30 }).map((_, index) => (
-        <button className="aspect-square bg-white border flex items-start p-2 md:p-5 text-base md:text-4xl font-black">
-          {index + 1}
-        </button>
-      ))}
+    <div className="h-full flex flex-col gap-6 py-10">
+      <div className="flex items-center justify-between px-10">
+        <h1 className="text-4xl md:text-7xl font-black text-center">
+          Caledário.ao - {date.getFullYear()}
+        </h1>
+        <div className="flex items-center justify-between gap-3">
+          <button className="text-xl capitalize cursor-pointer h-14 flex items-center justify-center bg-neutral-300 px-6 border rounded-xl">
+            {format(date, "MMMM", {
+              locale: pt,
+            })}
+          </button>
+          <button
+            onClick={handlePrevMonth}
+            className="size-14 bg-neutral-300 flex items-center justify-center rounded-xl border cursor-pointer"
+          >
+            <ArrowLeft />
+          </button>
+          <button
+            onClick={handleNextMonth}
+            className="size-14 bg-neutral-300 flex items-center justify-center rounded-xl border cursor-pointer"
+          >
+            <ArrowRight />
+          </button>
+        </div>
+      </div>
+      <hr />
+
+      <div>
+        <div className="grid grid-cols-7 border-collapse gap-4 px-3 md:px-10 pb-10">
+          {daysOfTheWeek.map((item) => (
+            <button className="w-full bg-neutral-200 border rounded-xl h-14">
+              {item}
+            </button>
+          ))}
+        </div>
+        <div className="grid grid-cols-7 border-collapse gap-4 px-3 md:px-10 pb-10">
+          {Array.from({ length: daysInMonth }).map((_, index) => (
+            <button
+              key={index}
+              className="aspect-square hover:scale-105 hover:shadow-2xl duration-75 transition-all rounded-2xl shadow-sm bg-neutral-200 cursor-pointer border flex items-start p-2 md:p-5 text-base md:text-4xl font-black"
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
